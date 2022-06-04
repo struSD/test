a4f2a5b985bfe0e2b5d814ba2ff8f5ac2135a700
Danylo Strus <danylo.strus@gmail.com>
2022-06-04 17:40:38 +0300

    add name

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, my name danylo!");
+
